5609cb1da62b90413bd34ca004502c1651fde991
Neville Nazerane <live.neville@live.com>
2023-07-09 18:13:59 -0500

    setup tester

--- src/Ip.Client/Program.cs
+++ src/Ip.Client/Program.cs
@@ -6,6 +6,19 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
+builder.Services.AddScoped(p => new ShowMe(builder.HostEnvironment.BaseAddress));
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
+
+class ShowMe
+{
+    public ShowMe(string urlBase)
+    {
+        UrlBase = urlBase;
+    }
+
+    public string UrlBase { get; }
+}
