04f70abcf16939a7d9248108570e290b80c1ccd8
Neville Nazerane <live.neville@live.com>
2023-08-05 19:50:56 -0700

    added a razor website

---
+++ src/Ip.Website/Pages/Index.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Ip.Website.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string IPv4Address { get; set; }
+        public string IPv6Address { get; set; }
+
+        public void OnGet()
+        {
+            if (HttpContext.Request.Headers.TryGetValue("X-Forwarded-For", out var forwardedFor))
+            {
+                foreach (var ip in forwardedFor.ToString().Split(','))
+                {
+                    if (System.Net.IPAddress.TryParse(ip.Trim(), out var ipAddress))
+                    {
+                        if (ipAddress.AddressFamily == AddressFamily.InterNetwork && string.IsNullOrEmpty(IPv4Address))
+                        {
+                            IPv4Address = ip.Trim();
+                        }
+                        else if (ipAddress.AddressFamily == AddressFamily.InterNetworkV6 && string.IsNullOrEmpty(IPv6Address))
+                        {
+                            IPv6Address = ip.Trim();
+                        }
+                    }
+                }
+            }
+
+            if (string.IsNullOrEmpty(IPv4Address) && HttpContext.Connection.RemoteIpAddress?.AddressFamily == AddressFamily.InterNetwork)
+            {
+                IPv4Address = HttpContext.Connection.RemoteIpAddress.ToString();
+            }
+
+            if (string.IsNullOrEmpty(IPv6Address) && HttpContext.Connection.RemoteIpAddress?.AddressFamily == AddressFamily.InterNetworkV6)
+            {
+                IPv6Address = HttpContext.Connection.RemoteIpAddress.ToString();
+            }
+        }
+
+    }
+}
---
+++ src/Ip.Website/Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
